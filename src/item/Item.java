d5c8b0d750581aac2943c660779dee190b1d4928
theandrewbalint <theandrewbalint@gmail.com>
2016-09-12 13:36:30 -0600

    added item objects, and added an item list to the player object.

---
+++ src/item/Item.java
@@ -0,0 +1,11 @@
+package item;
+
+import java.util.List;
+
+public class Item {
+
+	String name;
+	String description;
+	List<List<Character>> picture;
+	
+}
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,21 +1,29 @@
 package player;
 
+import java.util.List;
+import item.Item;
+
 public class Player {
 
 	Character playerIcon = '@';
 	Character replacedChar;
 	int playerXcoord;
 	int playerYcoord;
+	List<Item> itemList;
 	
 	
 	public void setPlayerIcon(Character playerIconIn){this.playerIcon = playerIconIn ;}
 	public void setReplacedChar(Character replacedCharIn){this.replacedChar = replacedCharIn ;}
 	public void setPlayerXCoord(int playerXcoordIn){this.playerXcoord = playerXcoordIn;}
 	public void setPlayerYCoord(int playerYcoordIn){this.playerYcoord = playerYcoordIn;}
+	public void setItemList(List<Item> itemListIn){this.itemList = itemListIn;}
 	
 	public Character gerPlayerIcon(){return this.playerIcon;}
 	public Character getReplacedChar(){return this.replacedChar;}
 	public int getPlayerXCoord(){return this.playerXcoord;}
 	public int getPlayerYCoord(){return this.playerYcoord;}
+	public List<Item> getItemList(){return this.itemList;}
+	
+	public void addItemToItemList(Item itemIn){this.itemList.add(itemIn);}
 	
 }
