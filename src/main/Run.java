e79e0165591856168f58c992815dbf69ed57a1e9
theandrewbalint <theandrewbalint@gmail.com>
2016-09-12 16:55:10 -0600

    Added interactable objects, added a fishing rod to the map, added fishing rod item that can be placed in a users inventory

--- src/main/Run.java
+++ src/main/Run.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 import javax.swing.JTextArea;
  
 import item.Item;
+import map.InteractableObject;
 import map.Map;
 import player.Player;
 
@@ -75,21 +76,40 @@ public class Run {
 	public static void initialSetup(Map map,JTextArea board,Player player)
 	{
 			
-		player.setPlayerXCoord(3);
-		player.setPlayerYCoord(3);
+		player.setPlayerXCoord(3);//set base x
+		player.setPlayerYCoord(3);//sent base y
+		
+		//====building a test item to put into the users inventory
 		Item log = new Item();
 		log.setName("Log");
 		log.setDescription("Its a log.");
 		log.setPicture("  _\n / /\n/ /");
-		List<Item> playerItemList = new ArrayList<Item>();
-		playerItemList.add(log);
-		player.setItemList(playerItemList);
+		List<Item> playerItemList = new ArrayList<Item>(); //make a new item list for the player
+		playerItemList.add(log);  //add the log to the item list
+		player.setItemList(playerItemList); //set that item list to the players
 
-		player.setReplacedChar(map.getCharacterList().get(player.getPlayerYCoord()).get(player.getPlayerXCoord()));
-		map.getCharacterList().get(player.getPlayerYCoord()).set(player.getPlayerXCoord(), player.gerPlayerIcon());
-		map.setMapString(convertMapToString(map.getCharacterList()));
+		//=====building a test item for interactable objects
+		InteractableObject fishingRod = new InteractableObject(); //set up a new object
+		fishingRod.setName("Worn Rod"); //set the name
+		Item rod = new Item(); //make a new item to test interactable
+		rod.setName("Worn Rod"); //anem it
+		rod.setDescription("An old fishing rod"); //describe it
+		rod.setPicture(" /\\\n.  \\\n    \\");	// draw it
+		fishingRod.setItem(rod);//add the item to the interactable object
+		List<Integer> rodCoords = new ArrayList<Integer>(); //make an arraylist for the coords
+		rodCoords.add(17); //add the x coord
+		rodCoords.add(16); //add y coord
+		fishingRod.setXYCoordinateList(rodCoords); // set the coords
 		
-		board.setText(map.getMapString());
+		List<InteractableObject> mapObjectList = new ArrayList<InteractableObject>(); // make new interactable list
+		map.setInteractableObjectList(mapObjectList); // set the list to the map
+		map.addToInteractableObjectList(fishingRod); // add the fishing rod to the object list
+		
+		player.setReplacedChar(map.getCharacterList().get(player.getPlayerYCoord()).get(player.getPlayerXCoord()));//get the first chracter the player will be overwritting
+		map.getCharacterList().get(player.getPlayerYCoord()).set(player.getPlayerXCoord(), player.gerPlayerIcon());//set the player icon on the map
+		map.setMapString(convertMapToString(map.getCharacterList()));//get the map
+		
+		board.setText(map.getMapString());//draw the map
 	}
 	
 	
@@ -153,6 +173,15 @@ public class Run {
 		        		player.setInInventory(false);
 		        	}
 		        	return;
+		        case KeyEvent.VK_SPACE:
+		        	if(player.getInInventory())
+		        	{
+		        		//do some stuff
+		        	}
+		        	else
+		        	{
+		        		interact(map, player);
+		        	}
 		        }
 		    }
 		    @Override
@@ -276,6 +305,24 @@ public class Run {
 		}
 		
 		return inventoryString;
+	}
+	
+	public static void interact(Map map, Player player)
+	{
+		
+		for(InteractableObject object : map.getInteractableObjectList())
+		{
+			if(player.getPlayerXCoord() == object.getXYCoordinateList().get(0) && player.getPlayerYCoord() == object.getXYCoordinateList().get(1))
+			{
+				if(object.getisItem())
+				{
+					player.addItemToItemList(object.getItem());
+					map.getInteractableObjectList().remove(object);
+					player.setReplacedChar(' ');
+				}
+			}
+		}
+		
 	}
 	
 }
--- src/map/Map.java
+++ src/map/Map.java
@@ -5,15 +5,19 @@ import java.util.List;
 public class Map {
 
 	List<List<Character>> characterList;
+	List<InteractableObject> interactableList;
 	String mapString;
 	String hitBoxCharacterList = "#\\/.|-";
 	
 	public List<List<Character>> getCharacterList(){return this.characterList;}
 	public String getMapString(){return this.mapString;}
 	public String getHitBoxCharacterList(){return this.hitBoxCharacterList;}
+	public List<InteractableObject> getInteractableObjectList(){return this.interactableList;}
 	
 	public void setCharacterList(List<List<Character>> characterListIn){this.characterList = characterListIn;}
 	public void setMapString(String mapStringIn){this.mapString = mapStringIn;}
+	public void setInteractableObjectList(List<InteractableObject> interactableObjectListIn){this.interactableList = interactableObjectListIn;}
 	
+	public void addToInteractableObjectList(InteractableObject object){this.interactableList.add(object);}
 	
 }
