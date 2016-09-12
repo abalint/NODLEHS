7cca79c261685327d84edae9fe861be6d34debd8
theandrewbalint <theandrewbalint@gmail.com>
2016-09-12 10:18:03 -0600

    Reorginization

--- src/display/Launch.java
+++ src/display/Launch.java
@@ -21,128 +21,26 @@ import player.Player;
 public class Launch {
 	
 	
-	public static void launch()
+	public static JTextArea launch()
 	{
 		
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());  //give your JPanel a BorderLayout
 
 		JTextArea text = new JTextArea(); 
-		text.setText("testaskldjfklajsdfkl;ajsdfk");
 		text.setEditable(false);
-		
-		Player player = new Player();	
-		player.setPlayerXCoord(3);
-		player.setPlayerYCoord(3);
-		
-		List<List<Character>> map = getMap();
-		player.setReplacedChar(map.get(player.getPlayerYCoord()).get(player.getPlayerXCoord()));
-		map.get(player.getPlayerYCoord()).set(player.getPlayerXCoord(), player.gerPlayerIcon());
-		String mapString = convertMapToString(map);
 		text.setFont(new Font("monospaced", Font.PLAIN, 12));
-		text.setText(mapString);
-		
-		//===key listener
-		text.addKeyListener(new KeyListener(){
-		    @Override
-		    public void keyPressed(KeyEvent e){
-		        switch (e.getKeyCode()){
-		        
-		        case KeyEvent.VK_W:
-		        	text.setText("YOU PRESSED W");
-		        	break;
-		        case KeyEvent.VK_S:
-		        	text.setText("YOU PRESSED S");
-		        	break;
-		        case KeyEvent.VK_A:
-		        	text.setText("YOU PRESSED A");
-		        	break;
-		        case KeyEvent.VK_D:
-		        	//map = moveToRight(map, player);
-		        	text.setText(convertMapToString(map));
-		        	break;
-		        case KeyEvent.VK_I:
-		        	text.setText("YOU PRESSED I");
-		        	break;
-		        }
-		    }
-		    @Override
-		    public void keyTyped(KeyEvent e) {
-		    }
-		    @Override
-		    public void keyReleased(KeyEvent e) {
-		    }
-		});
-		//===key listener
-		System.out.println("test");
-		
-		
 		JScrollPane scroll = new JScrollPane(text); //place the JTextArea in a scroll pane
 		panel.add(scroll, BorderLayout.CENTER); //add the JScrollPane to the panel
 		// CENTER will use up all available space
-		
 		JFrame frame = new JFrame();
 		frame.add(panel);
 		frame.setVisible(true);
 		frame.setSize(500, 500);
 		frame.setDefaultCloseOperation(3);
 		
+		return text;
+		
 	}
 	
-	public static List<List<Character>> getMap()
-	{
-		List<List<Character>> map = new ArrayList<List<Character>>();
-		
-		Scanner scanner = null;
-		try {
-			scanner = new Scanner(new FileReader("C:/Users/andrew.balint.SATS/workspace/NODLEHS/src/Files/map.txt")); 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-			return null;
-		}
-		
-		while(scanner.hasNextLine())
-		{
-			List<Character>mapLine = new ArrayList<Character>();
-			String line = scanner.nextLine();
-			for(int i = 0; i < line.length(); i ++)
-			{
-				mapLine.add(line.charAt(i));
-			}
-			map.add(mapLine);
-		}
-		
-		return map;
-	}//end getMap
-	
-	public static String convertMapToString(List<List<Character>> map)
-	{
-		String mapString = new String();
-		
-		
-		for(List<Character> mapLine : map)
-		{
-			for(Character mapChar : mapLine)
-			{
-				mapString += mapChar;
-			}
-			mapString += "\n";
-		}
-		
-		return mapString;
-	}
-	
-	public static List<List<Character>> moveToRight(List<List<Character>> map, Player player)
-	{
-		
-		if(map.get(player.getPlayerYCoord()).size() -1 == player.getPlayerXCoord())
-			return map;
-		
-		Character tempChar = player.getReplacedChar();
-		player.setReplacedChar(map.get(player.getPlayerYCoord()).get(player.getPlayerXCoord()+1));
-		map.get(player.getPlayerYCoord()).set(player.getPlayerXCoord()+1, player.gerPlayerIcon());
-		
-		return map;
-	}
-
 }
--- src/main/Run.java
+++ src/main/Run.java
@@ -1,10 +1,171 @@
 package main;
 
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JTextArea;
+
+import player.Player;
+
 public class Run {
 
 	public static void main(String[] args)
-	{
+	{		
 		System.out.println("hello");
-		display.Launch.launch();
+//		JTextArea board = display.Launch.launch();
+//		List<List<Character>> map = getMap();
+//		Player player = new Player();
+//		initialSetup(map, board, player);
+//		double time_passed = 0;
+//		double delta_time = 0;
+//		int i = 0;
+//			while (true) { // keep running
+//		    	long startLoopTime = Calendar.getInstance().getTimeInMillis(); 
+//		   
+//		        // update game logic once for every tick passed
+//		  
+//		        while (time_passed >= .01) {
+//		        	
+//		            listener(board, player, map , i);
+//		            time_passed -= .01;
+//		            // You might limit the number of iterations here (like 10)
+//		            // to not get stuck due to processing taking too long (and time adding up)
+//		        }
+//
+//		        // draw screen content
+//		        //drawStuff(delta_time);
+//
+//		        // update timing
+//		    	long endLoopTime = Calendar.getInstance().getTimeInMillis(); 
+//		        delta_time = startLoopTime - endLoopTime;
+//		        time_passed += delta_time;
+//		        i++;
+//		       
+//		    }
 	}
+	
+	
+	public static void initialSetup(List<List<Character>> map,JTextArea board,Player player)
+	{
+			
+		player.setPlayerXCoord(3);
+		player.setPlayerYCoord(3);
+
+		player.setReplacedChar(map.get(player.getPlayerYCoord()).get(player.getPlayerXCoord()));
+		map.get(player.getPlayerYCoord()).set(player.getPlayerXCoord(), player.gerPlayerIcon());
+		String mapString = convertMapToString(map);
+		
+		board.setText(mapString);
+		
+	}
+	
+	
+	public static void listener (JTextArea board, Player player, List<List<Character>> map, int i)
+	{
+		
+		
+		board.setText(""+i);
+//		board.addKeyListener(new KeyListener(){
+//		    @Override
+//		    public void keyPressed(KeyEvent e){
+//		        switch (e.getKeyCode()){
+//		        
+//		        case KeyEvent.VK_W:
+//		        	board.setText("YOU PRESSED W");
+//		        	break;
+//		        case KeyEvent.VK_S:
+//		        	board.setText("YOU PRESSED S");
+//		        	break;
+//		        case KeyEvent.VK_A:
+//		        	board.setText("YOU PRESSED A");
+//		        	break;
+//		        case KeyEvent.VK_D:
+//		        	List<List<Character>> reMap = moveToRight(map, player);
+//		        	String reMapString = convertMapToString(reMap);
+//		        	board.setText(reMapString);
+//		        	//text.setText(convertMapToString(map));
+//		        	break;
+//		        case KeyEvent.VK_I:
+//		        	board.setText("YOU PRESSED I");
+//		        	break;
+//		        }
+//		    }
+//		    @Override
+//		    public void keyTyped(KeyEvent e) {
+//		    }
+//		    @Override
+//		    public void keyReleased(KeyEvent e) {
+//		    }
+//		});
+	}
+	
+	
+	public static List<List<Character>> getMap()
+	{
+		List<List<Character>> map = new ArrayList<List<Character>>();
+		
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new FileReader("C:/Users/andrew.balint.SATS/workspace/NODLEHS/src/Files/map.txt")); 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		while(scanner.hasNextLine())
+		{
+			List<Character>mapLine = new ArrayList<Character>();
+			String line = scanner.nextLine();
+			for(int i = 0; i < line.length(); i ++)
+			{
+				mapLine.add(line.charAt(i));
+			}
+			map.add(mapLine);
+		}
+		
+		return map;
+	}//end getMap
+	
+	public static String convertMapToString(List<List<Character>> map)
+	{
+		String mapString = new String();
+		
+		
+		for(List<Character> mapLine : map)
+		{
+			for(Character mapChar : mapLine)
+			{
+				mapString += mapChar;
+			}
+			mapString += "\n";
+		}
+		
+		return mapString;
+	}
+	
+	public static List<List<Character>> moveToRight(List<List<Character>> map, Player player)
+	{
+		
+		if(map.get(player.getPlayerYCoord()).size() -1 == player.getPlayerXCoord())
+			return map;
+		
+		Character tempChar = player.getReplacedChar();
+		player.setReplacedChar(map.get(player.getPlayerYCoord()).get(player.getPlayerXCoord()+1));
+		map.get(player.getPlayerYCoord()).set(player.getPlayerXCoord()+1, player.gerPlayerIcon());
+		
+		return map;
+	}
+
+	
+	
+	
 }
